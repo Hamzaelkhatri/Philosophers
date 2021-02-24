487f7601bb0c5bf947235f33f26028c8b7c6ead1
hamzaelkhatri <hamzaelkhatri@gmail.com>
2021-02-24 16:49:16 +0000

    update

--- philo.c
+++ philo.c
@@ -6,8 +6,8 @@
 #include <sys/time.h>
 
 
-pthread_t Philosophers[5];
-pthread_mutex_t chopsticks[5];
+pthread_t Philosophers[50];
+pthread_mutex_t chopsticks[50];
 struct timeval current_time,start;
 
 void *func(void *val)
@@ -16,41 +16,55 @@ void *func(void *val)
     printf("Philosopher %d is thinking\n", i);
     pthread_mutex_lock(&chopsticks[i]);
     gettimeofday(&current_time, NULL);
-    printf("%ld ms Philosopher %d has taking a fork\n",(current_time.tv_sec * 1000000 + current_time.tv_usec) - (start.tv_sec + start.tv_usec),i);
-    pthread_mutex_lock(&chopsticks[(i + 1) % 5]);
+    printf("%ld ms Philosopher %d has taking a fork\n",-1 * ((start.tv_sec-current_time.tv_sec)*1000000 + start.tv_usec - current_time.tv_usec),i);
+    pthread_mutex_lock(&chopsticks[(i + 1) % 50]);
     gettimeofday(&current_time, NULL);
-    printf("%ld ms Philosopher %d has taking a fork\n",(current_time.tv_sec * 1000000 + current_time.tv_usec) - (start.tv_sec + start.tv_usec),i);
+    printf("%ld ms Philosopher %d has taking a fork\n",-1 *((start.tv_sec-current_time.tv_sec)*1000000 + start.tv_usec - current_time.tv_usec),i);
     printf("Philosopher %d is eating\n",i);
     sleep(3);
      gettimeofday(&current_time, NULL);
-    int sleeptime = (current_time.tv_sec * 1000000 + current_time.tv_usec) - (start.tv_sec + start.tv_usec);
     pthread_mutex_unlock(&chopsticks[i]);
-    pthread_mutex_unlock(&chopsticks[(i + 1) % 5]);
-    printf("Philosopher %d finished eating in %d ms\n",i,sleeptime);
+    pthread_mutex_unlock(&chopsticks[(i + 1) % 50]);
+    printf("Philosopher %d finished eating in %ld ms\n",i,-1 * ((start.tv_sec-current_time.tv_sec)*1000000 + start.tv_usec - current_time.tv_usec));
     return (NULL);
 }
 
-int main()
+int check_args(char **ag)
+{
+    int i = 0;
+    while (ag[i])
+    {
+        /* code */
+    }
+    
+}
+
+int main(int ac,char **ag)
 {
     int i;
 
+    if(ac < 5 || ac > 6)
+    {
+        printf("ARGS : [ NUMBER_OF_PHIL | TIME_TO_DIE | TIME_TO_EAT | TIME_TO_SLEEP | NUMBER_OF_PHILOSOPHER_MUST_EAT ]");
+        exit(0);
+    }
     i = 0;
     gettimeofday(&start, NULL);
-    while (i < 5)
+    while (i < 50)
     {
         pthread_mutex_init(&chopsticks[i], NULL);
         i++;
     }
 
     i = 0;
-    while (i < 5)
+    while (i < 50)
     {
         pthread_create(&Philosophers[i], NULL, (void *)func, (void *)(long)i);
         i++;
     }
 
     i = 0;
-    while (i < 5)
+    while (i < 50)
     {
         pthread_join(Philosophers[i], NULL);
         i++;
