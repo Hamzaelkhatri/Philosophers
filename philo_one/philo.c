0260edf2aa94a729ec70e063622f4fbbf4dca5b6
hamza <hamzaelkhatri@gmail.com>
2021-04-27 23:27:20 +0000

    fix die mode

--- philo_one/philo.c
+++ philo_one/philo.c
@@ -1,5 +1,22 @@
 #include "philosophers.h"
 
+void is_dead(t_philo *philo)
+{
+    int i;
+
+    i = 0;
+    while (i < philo->number_phil)
+    {
+        if (get_current() - philo->times[i]->last_time_eat > philo->time_to_die)
+        {
+            printf("\e[0;33m %ld ms %d is die :(\n", get_current() - philo->current_time, i);
+            exit(1);
+        }
+        usleep(20);
+        i++;
+    }
+}
+
 void *func(void *val)
 {
     t_philo *philo;
@@ -23,43 +40,52 @@ void *func(void *val)
         }
         i = 0;
     }
+    is_dead(philo);
     if (philo->state[i] == NAN || philo->state[i] == THINKING || philo->state[i] == SLEEPING)
     {
+        is_dead(philo);
         printf("\e[0;31m %d ms Philosopher %d is thinking\n", get_current() - philo->current_time, i);
         philo->state[i] = THINKING;
     }
     if (!pthread_mutex_lock(&philo->forks[i]))
     {
         printf("\e[0;32m %d ms Philosopher %d has taking a fork 1\n", get_current() - philo->current_time, i);
+        // is_dead(philo);
         j++;
     }
     if (!pthread_mutex_lock(&philo->forks[(i + 1) % philo->number_phil]))
     {
         printf("\e[0;32m %ld ms Philosopher %d has taking a fork 2\n", get_current() - philo->current_time, i);
         j++;
+        // is_dead(philo);
+        // usleep(20);
     }
     if (j == 2)
     {
         printf("\e[1;34m %ld ms Philosopher %d is eating\n", get_current() - philo->current_time, i);
         philo->state[i] = EAT;
         usleep(philo->time_to_eat * 1000);
+        philo->times[i]->last_time_eat = get_current();
         pthread_mutex_unlock(&philo->forks[i]);
         pthread_mutex_unlock(&philo->forks[(i + 1) % philo->number_phil]);
+        // usleep(20);
         printf("\e[0;35m %ld ms Philosopher %d is sleeping\n", get_current() - philo->current_time, i);
         philo->state[i] = SLEEPING;
+        is_dead(philo);
         usleep(philo->time_to_sleep * 1000);
-        philo->times[i]->last_time_eat = get_current();
         philo->state[i] = NAN;
         j = 0;
     }
-    if (get_current() - philo->times[i]->last_time_eat > philo->time_to_die)
-    {
-        // printf("[%ld last time]", get_current() - philo->times[i]->last_time_eat, i);
-        pthread_mutex_lock(&philo->died);
-        printf("\e[0;33m %ld ms %d is die :(\n", get_current() - philo->current_time, i);
-        pthread_mutex_unlock(&philo->died);
-        exit(1);
-    }
+    is_dead(philo);
+    // if (get_current() - philo->times[i]->last_time_eat > philo->time_to_die)
+    // {
+    //     // printf("[%ld last time]", get_current() - philo->times[i]->last_time_eat, i);
+    //     pthread_mutex_lock(&philo->died);
+    //     printf("\e[0;33m %ld ms %d is die :(\n", get_current() - philo->current_time, i);
+    //     pthread_mutex_unlock(&philo->died);
+    //     usleep(20);
+    //     exit(1);
+    // }
     func(philo);
     return (val);
 }
@@ -95,10 +121,9 @@ void do_stuff(t_philo *philo)
     {
         pthread_create(&philo->Philosophers[i], NULL, (void *)func, (void *)philo);
         i++;
+        usleep(20);
     }
     i = 0;
-    // while (i < philo->number_phil)
-    // pthread_detach(philo->Philosophers[i++]);
 }
 
 void waiting_threads(t_philo *philo)
